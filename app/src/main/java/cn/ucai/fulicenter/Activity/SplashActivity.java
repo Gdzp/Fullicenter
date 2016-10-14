87462195d2b5ec813ff0db41d841ee97f36de106
YourName <email@example.com>
2016-10-14 17:23:29 +0800

    实现闪屏和动画，图片

---
+++ app/src/main/java/cn/ucai/fulicenter/Activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package cn.ucai.fulicenter.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final long sleepTime=200;
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start=System.currentTimeMillis();
+                long costTime=System.currentTimeMillis()-start;
+                if(sleepTime-costTime>0){
+                    try {
+                        Thread.sleep(sleepTime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        }).start();
+
+    }
+
+
+}
--- app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
+++ app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
@@ -3,14 +3,13 @@ package cn.ucai.fulicenter.utils;
 import android.app.Activity;
 import android.content.Intent;
 
+import cn.ucai.fulicenter.Activity.MainActivity;
 import cn.ucai.fulicenter.R;
-import cn.ucai.fulicenter.activity.MainActivity;
-
 
 public class MFGT {
     public static void finish(Activity activity){
         activity.finish();
-        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
     }
     public static void gotoMainActivity(Activity context){
         startActivity(context, MainActivity.class);
