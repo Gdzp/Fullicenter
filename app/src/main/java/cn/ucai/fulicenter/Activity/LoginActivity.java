ec23096145f7183a9c87b000a284b7a7f8482e4a
YourName <email@example.com>
2016-10-23 00:32:45 +0800

    添加注册页面，并绑定登录页面的跳转

---
+++ app/src/main/java/cn/ucai/fulicenter/Activity/LoginActivity.java
@@ -0,0 +1,51 @@
+package cn.ucai.fulicenter.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ucai.fulicenter.R;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @BindView(R.id.username)
+    EditText username;
+    @BindView(R.id.password)
+    EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                break;
+            case R.id.btn_register:
+                break;
+        }
+    }
+}
