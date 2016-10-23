78fcb2111e1ac067659b50dcda9ba426692bbb9f
YourName <email@example.com>
2016-10-23 19:44:31 +0800

    注册成功之后将用户名带到登录界面

--- app/src/main/java/cn/ucai/fulicenter/Activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/Activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -7,7 +8,9 @@ import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.MFGT;
 
 public class LoginActivity extends BaseActivity {
 
@@ -44,8 +47,20 @@ public class LoginActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.btn_login:
                 break;
-            case R.id.btn_register:
+          case R.id.btn_register:
+              MFGT.gotoRegister(this);
                 break;
         }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RESULT_OK&&requestCode== I.REQUEST_CODE_REGISTER){
+        String name=data.getStringExtra(I.User.USER_NAME);
+            username.setText(name);
+        }
+
     }
 }
