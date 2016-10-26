d0a8db1afd1f8f8013bdd5d07267f3a58cd1753c
YourName <email@example.com>
2016-10-26 19:43:58 +0800

    商品详情页面商品收藏操作

--- app/src/main/java/cn/ucai/fulicenter/fragment/PresonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/fragment/PresonalCenterFragment.java
@@ -5,7 +5,10 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -14,26 +17,40 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.Activity.MainActivity;
 import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.bean.Result;
 import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.dao.UserDao;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.ImageLoader;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
-
+import cn.ucai.fulicenter.utils.ResultUtils;
 
 
 /**
  * Created by Think on 2016/10/24.
  */
 
-public class PresonalCenterFragment extends BaseFragment{
+public class PresonalCenterFragment extends BaseFragment {
     private static final String TAG = PresonalCenterFragment.class.getSimpleName();
     @BindView(R.id.iv_user_avatar)
     ImageView mIvUserAvatar;
     @BindView(R.id.tv_user_name)
     TextView mTvUserName;
-    User user;
-
+    User user = null;
     MainActivity mContext;
+    @BindView(R.id.center_user_info)
+    RelativeLayout centerUserInfo;
+    @BindView(R.id.tv_collect_count)
+    TextView tvCollectCount;
+    @BindView(R.id.center_user_order_lis)
+    GridView centerUserOrderLis;
+    @BindView(R.id.layout_center_collect)
+    LinearLayout layoutCenterCollect;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_personal_center, container, false);
@@ -51,11 +68,11 @@ public class PresonalCenterFragment extends BaseFragment{
     @Override
     protected void initData() {
         user = FuLiCenterApplication.getUser();
-        L.e(TAG,"user="+user);
-        if(user==null){
+        L.e(TAG, "user=" + user);
+        if (user == null) {
             MFGT.gotoLogin(mContext);
-        }else{
-            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,mIvUserAvatar);
+        } else {
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), mContext, mIvUserAvatar);
             mTvUserName.setText(user.getMuserNick());
         }
     }
@@ -64,6 +81,7 @@ public class PresonalCenterFragment extends BaseFragment{
     protected void setListener() {
 
     }
+
     @Override
 
     public void onResume() {
@@ -73,16 +91,70 @@ public class PresonalCenterFragment extends BaseFragment{
         if (user != null) {
             ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), mContext, mIvUserAvatar);
             mTvUserName.setText(user.getMuserNick());
+            syncUserInfo();
+            syncCollectsCount();
         }
     }
+    @OnClick(R.id.layout_center_collect)
+    public void gotoCollectsList(){
+        MFGT.gotoCollects(mContext);
+    }
 
 
-    @OnClick({R.id.tv_center_settings,R.id.center_user_info})
-  public void gotoSetting(){
+    @OnClick({R.id.tv_center_settings, R.id.center_user_info})
+    public void gotoSetting() {
         MFGT.gotoSettings(mContext);
     }
-    private void initOrderList(){
+
+    private void initOrderList() {
 
     }
 
+    public void syncUserInfo() {
+        NetDao.syncUserInfo(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                Result result = ResultUtils.getResultFromJson(s, User.class);
+                if (result != null) {
+                    User u = (User) result.getRetData();
+                    if (!user.equals(u)) {
+                        UserDao dao = new UserDao(mContext);
+                        boolean b = dao.saveUser(u);
+                        if (b) {
+                            FuLiCenterApplication.setUser(u);
+                            user = u;
+                            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), mContext, mIvUserAvatar);
+                            mTvUserName.setText(user.getMuserNick());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+
+    }
+
+    private void syncCollectsCount() {
+        NetDao.getCollectsCount(mContext, user.getMuserName(), new OkHttpUtils.OnCompleteListener<MessageBean>() {
+            @Override
+            public void onSuccess(MessageBean result) {
+                if (result != null && result.isSuccess()) {
+                    tvCollectCount.setText(result.getMsg());
+                } else {
+                    tvCollectCount.setText(String.valueOf(0));
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                tvCollectCount.setText(String.valueOf(0));
+                L.e(TAG, "error=" + error);
+
+            }
+        });
+    }
 }
