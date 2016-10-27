80df8588b280398ddf527dc7545165e8df2c4dd2
YourName <email@example.com>
2016-10-27 20:46:58 +0800

    添加购物车数量合算

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -6,7 +6,6 @@ import java.io.File;
 
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.bean.BoutiqueBean;
-import cn.ucai.fulicenter.bean.CartBean;
 import cn.ucai.fulicenter.bean.CategoryChildBean;
 import cn.ucai.fulicenter.bean.CategoryGroupBean;
 import cn.ucai.fulicenter.bean.CollectBean;
@@ -177,11 +176,11 @@ public static void deleteCollect(Context context,String username,int
                 .targetClass(MessageBean.class)
                 .execute(listener);
     }
-    public static void downloadCart(Context context, String username, OkHttpUtils.OnCompleteListener<CartBean[]>listener){
-            OkHttpUtils<CartBean[]> utils=new OkHttpUtils<>(context);
+    public static void downloadCart(Context context, String username, OkHttpUtils.OnCompleteListener<String>listener){
+            OkHttpUtils<String> utils=new OkHttpUtils<>(context);
             utils.setRequestUrl(I.REQUEST_FIND_CARTS)
                     .addParam(I.Cart.USER_NAME,username)
-                    .targetClass(CartBean[].class)
+                    .targetClass(String.class)
                     .execute(listener);
     }
 
