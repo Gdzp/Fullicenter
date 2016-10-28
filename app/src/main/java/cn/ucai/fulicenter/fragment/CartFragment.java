95b927e8a77d87bfb54d2a61f97a741bae0fdc3c
YourName <email@example.com>
2016-10-28 13:27:32 +0800

    购物车点击商品进入商品详情添加商品成功

--- app/src/main/java/cn/ucai/fulicenter/fragment/CartFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/fragment/CartFragment.java
@@ -206,4 +206,10 @@ public class CartFragment extends BaseFragment {
             mContext.unregisterReceiver(mReceiver);
         }
     }
+    @Override
+    public void onResume(){
+        super.onResume();
+        L.e(TAG,"onResume.....");
+        initData();
+    }
 }
